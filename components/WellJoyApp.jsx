d735d2512b775bb24c5447acf3201efd66a9ebc8
autoraven <71033102+autoraven@users.noreply.github.com>
2026-06-22 14:50:14 +0700

    sign

--- components/WellJoyApp.jsx
+++ components/WellJoyApp.jsx
@@ -1857,7 +1857,7 @@ const HRDMore = ({ user, showToast, onLogout, dbData, refreshData }) => {
         ))}
         <button onClick={()=>setShowChangePw(true)} style={{ padding:14,borderRadius:14,color:'#444',fontWeight:700,fontSize:14,border:'1px solid #e0e0e0',background:'white',cursor:'pointer',marginTop:4,width:'100%' }}>🔑 Ubah Password</button>
         <button onClick={onLogout} style={{ padding:14,borderRadius:14,color:'#E53935',fontWeight:700,fontSize:14,border:'2px solid #FFCDD2',background:'white',cursor:'pointer',marginTop:4,width:'100%' }}>← Keluar</button>
-        <p style={{ textAlign:'center',fontSize:10,color:'#ddd',marginTop:8 }}>#gg</p>
+        <p style={{ textAlign:'center',fontSize:10,color:'#ddd',marginTop:8 }}>created by #gg & Rifqi</p>
       </div>
 
       {showChangePw && <ChangePasswordModal user={user} showToast={showToast} onClose={()=>setShowChangePw(false)}/>}
